4777c46c9cbe1d420dd0d1c4eed3f0c327f2e9f0
christophermilan <59456681+christophermilan@users.noreply.github.com>
2021-03-29 23:43:24 -0400

    Initial commit

---
+++ src/Pokedex.js
@@ -0,0 +1,46 @@
+import React, { useState, useRef } from "react";
+import { PokemonType } from "./PokemonType";
+
+export function Pokedex() {
+  const [selectedPokemon, setSelectedPokemon] = useState(undefined);
+  const searchBox = useRef(null);
+
+  async function getPokemonInfo(name) {
+    const URL = `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`;
+
+    const response = await fetch(URL);
+    if (!response.ok) {
+      alert("Pokemon does not exist");
+      return;
+    }
+    const data = await response.json();
+
+    setSelectedPokemon(data);
+  }
+
+  return (
+    <div>
+      {selectedPokemon && selectedPokemon.species && (
+        <div>
+          <h4>{selectedPokemon.species.name}</h4>
+
+          <PokemonType type={selectedPokemon.types[0].type.name} />
+          {selectedPokemon.types.length > 1 && (
+            <PokemonType type={selectedPokemon.types[1].type.name} />
+          )}
+
+          <img src={selectedPokemon.sprites.front_default} alt="sprite" />
+        </div>
+      )}
+
+      <p>Select Pokemon:</p>
+      <button onClick={() => getPokemonInfo("squirtle")}>Squirtle</button>
+      <button onClick={() => getPokemonInfo("bulbasaur")}>bulbasaur</button>
+      <button onClick={() => getPokemonInfo("charmander")}>Charmander</button>
+      <input ref={searchBox} />
+      <button onClick={() => getPokemonInfo(searchBox.current.value)}>
+        Search{" "}
+      </button>
+    </div>
+  );
+}
